f889955664a504e06e2c4fb5bf3c786820b9e6a1
Vishal Vinay Ram <vishalvinayram5432@gmail.com>
2023-01-11 19:27:46 +0530

    password_reset

--- dbms/urls.py
+++ dbms/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.views import *
 from soliders.views import *
 from terror.views import *
 from django.conf.urls import include
@@ -25,6 +25,8 @@ from regiment import urls as regimenturls
 from ammon import urls as ammourls
 from terror import urls as terrorurls
 from missions import urls  as missionurls
+from django.contrib.auth import views as auth_views
+
 
 
 
@@ -45,7 +47,10 @@ urlpatterns = [
     # path('logout/',logouts,name="logout"),
     # path('login/',LoginView.as_view(template_name='forms/login.html'),name='login'),
     path('logout/', LogoutView.as_view(template_name ="forms/logout.html"), name='logout'),
-
+    path('reset_password/',auth_views.PasswordResetView.as_view(template_name='forms/password/reset_password.html'), name='password_reset'),
+   path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='forms/password/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="forms/password/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='forms/password/password_reset_complete.html'), name='password_reset_complete'),      
     
 
 
